ce34cb6fde08019f1420f6487ee5498aa8241df1
sophkim <soph.b.kim@gmail.com>
2023-01-13 17:37:27 +0900

    네비바 수정중

--- src/App.js
+++ src/App.js
@@ -1,9 +1,128 @@
-import Main from "./pages/Main";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./pages/home/Main";
+import Greeting from "./pages/about/Greeting";
+import Patent from "./pages/about/Patent";
+import Location from "./pages/about/Location";
+import Contact from "./pages/about/Contact";
+import Download from "./pages/download/Download";
+import Support from "./pages/support/Support";
+import Delivery from "./pages/partner/Delivery";
+import Partners from "./pages/partner/Partners";
+import SafetyHelmetDetection from "./pages/ai/SafetyHelmetDetection";
+import GrindingDetection from "./pages/ai/GrindingDetection";
+import SmartGauge from "./pages/ai/SmartGauge";
+import Furnace from "./pages/ai/Furnace";
+import ExpansionJoint from "./pages/ai/ExpansionJoint";
+import PID from "./pages/ai/PID";
+import VisionEngineering from "./pages/ai/VisionEngineering";
+import PortableIteCamera from "./pages/product/PortableIteCamera";
+import AiSmartGaugeReader from "./pages/product/AiSmartGaugeReader";
+import OctoportablePole from "./pages/product/OctoportablePole";
+import OctoportableStandard from "./pages/product/OctoportableStandard";
+import OctoportableCompact from "./pages/product/OctoportableCompact";
+import OctoThermalWiFi from "./pages/product/OctoThermalWiFi";
+import OctoFLY from "./pages/product/OctoFLY";
+import IPTV from "./pages/product/IPTV";
+import IpSpeaker from "./pages/product/IpSpeaker";
+import IpSpeakerServer from "./pages/product/IpSpeakerServer";
+import IpSpeakerBroadcastDevice from "./pages/product/IpSpeakerBroadcastDevice";
+import OctopusStreamer from "./pages/product/OctopusStreamer";
+import OctopusCMS from "./pages/product/OctopusCMS";
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+
+          {/* 회사소개 */}
+          <Route path="/about/greeting" element={<Greeting />} />
+          <Route path="/about/patent" element={<Patent />} />
+          <Route path="/about/location" element={<Location />} />
+          <Route path="/about/contact" element={<Contact />} />
+
+          {/* 제품소개 */}
+          <Route
+            path="/product/procurement/portable-lte-camera"
+            element={<PortableIteCamera />}
+          />
+          <Route
+            path="/product/procurement/ai-smart-gauge-reader"
+            element={<AiSmartGaugeReader />}
+          />
+          <Route
+            path="/product/portable-lte-camera/standard"
+            element={<OctoportableStandard />}
+          />
+          <Route
+            path="/product/portable-lte-camera/compact"
+            element={<OctoportableCompact />}
+          />
+          <Route
+            path="/product/portable-lte-camera/pole"
+            element={<OctoportablePole />}
+          />
+          <Route
+            path="/product/portable-lte-camera/wifi"
+            element={<OctoThermalWiFi />}
+          />
+          <Route
+            path="/product/portable-lte-camera/fly"
+            element={<OctoFLY />}
+          />
+          <Route
+            path="/product/ai/smart-gauge-reader"
+            element={<AiSmartGaugeReader />}
+          />
+          <Route path="/product/iptv/iptv" element={<IPTV />} />
+          <Route
+            path="/product/ip-speaker/ip-speaker"
+            element={<IpSpeaker />}
+          />
+          <Route
+            path="/product/ip-speaker/ip-speaker-server"
+            element={<IpSpeakerServer />}
+          />
+          <Route
+            path="/product/ip-speaker/ip-speaker-broadcast-device"
+            element={<IpSpeakerBroadcastDevice />}
+          />
+          <Route
+            path="/product/video-solution/streamer"
+            element={<OctopusStreamer />}
+          />
+          <Route path="/product/video-solution/cms" element={<OctopusCMS />} />
+
+          {/* AI개발 */}
+          <Route
+            path="/ai/solution/safety-helmet-detection"
+            element={<SafetyHelmetDetection />}
+          />
+          <Route
+            path="/ai/solution/grinding-detection"
+            element={<GrindingDetection />}
+          />
+          <Route path="/ai/solution/smart-gauge" element={<SmartGauge />} />
+          <Route path="/ai/r&d/furnace" element={<Furnace />} />
+          <Route path="/ai/r&d/expansion-joint" element={<ExpansionJoint />} />
+          <Route path="/ai/r&d/p&id" element={<PID />} />
+          <Route
+            path="/ai/vision-engineering"
+            element={<VisionEngineering />}
+          />
+
+          {/* 파트너 */}
+          <Route path="/partner/delivery" element={<Delivery />} />
+          <Route path="/partner/parners" element={<Partners />} />
+
+          {/* 고객지원 */}
+          <Route path="/support" element={<Support />} />
+
+          {/* 다운로드 */}
+          <Route path="/download" element={<Download />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -46,12 +46,16 @@ function Navbar(props) {
       <nav className={styles.navbar}>
         <div className={styles.logo}>
           {" "}
-          <a href="https://www.cnsi.kr">
+          <a href="/">
             <img src={logo} className={styles.logoImage} alt="" />
           </a>
         </div>
         <ul className={styles.naviMenu}>
-          <li onMouseEnter={handleIntro} onMouseLeave={handleIntroClose}>
+          <li
+            className={styles.test}
+            onMouseEnter={handleIntro}
+            onMouseLeave={handleIntroClose}
+          >
             회사소개
           </li>
           <li onMouseEnter={handleProduct} onMouseLeave={handleProductClose}>
@@ -66,8 +70,12 @@ function Navbar(props) {
           <li onMouseEnter={handlePartner} onMouseLeave={handlePartnerClose}>
             파트너
           </li>
-          <li>고객지원</li>
-          <li>다운로드</li>
+          <a href="/support">
+            <li>고객지원</li>
+          </a>
+          <a href="/download">
+            <li>다운로드</li>
+          </a>
         </ul>
         <ul className={styles.naviUtill}>
           <li>
@@ -88,18 +96,26 @@ function Navbar(props) {
           onMouseEnter={handleIntro}
           onMouseLeave={handleIntroClose}
         >
-          <ul>
-            <p>인사말</p>
-          </ul>
-          <ul>
-            <p>특허 및 소프트웨어 저작권</p>
-          </ul>
-          <ul>
-            <p>오시는길</p>
-          </ul>
-          <ul>
-            <p>연락처</p>
-          </ul>
+          <a href="/about/greeting">
+            <ul>
+              <p>인사말</p>
+            </ul>
+          </a>
+          <a href="/about/patent">
+            <ul>
+              <p>특허 및 소프트웨어 저작권</p>
+            </ul>
+          </a>
+          <a href="/about/location">
+            <ul>
+              <p>오시는길</p>
+            </ul>
+          </a>
+          <a href="/about/contact">
+            <ul>
+              <p>연락처</p>
+            </ul>
+          </a>
         </ul>
       ) : (
         ""
@@ -114,37 +130,68 @@ function Navbar(props) {
         >
           <ul>
             <p>조달제품</p>
-            <li>LTE 이동형 현장 관제 카메라</li>
-            <li>AI 스마트 게이지 리더</li>
+            <a href="/product/procurement/portable-lte-camera">
+              <li>LTE 이동형 현장 관제 카메라</li>
+            </a>
+            <a href="/product/procurement/ai-smart-gauge-reader">
+              <li>AI 스마트 게이지 리더</li>
+            </a>
           </ul>
 
           <ul>
             <p>LTE 이동형 카메라</p>
-            <li>Octoportable Standard</li>
-            <li>Octoportable Compact</li>
+            <a href="/product/portable-lte-camera/standard">
+              <li>Octoportable Standard</li>
+            </a>
+            <a href="/product/portable-lte-camera/compact">
+              <li>Octoportable Compact</li>
+            </a>
+            <a href="/product/portable-lte-camera/pole">
+              <li>Octoportable Pole</li>
+            </a>
+            <a href="/product/portable-lte-camera/wifi">
+              <li>Octoportable Thermal WiFi</li>
+            </a>
+            <a href="/product/portable-lte-camera/fly">
+              <li>Octo Fly</li>
+            </a>
           </ul>
 
           <ul>
             <p>AI</p>
-            <li>AI 스마트 게이지 리더</li>
+            <a href="/product/ai/smart-gauge-reader">
+              <li>AI 스마트 게이지 리더</li>
+            </a>
           </ul>
 
           <ul>
             <p>IPTV</p>
-            <li>IPTV</li>
+            <a href="/product/iptv/iptv">
+              <li>IPTV</li>
+            </a>
           </ul>
 
           <ul>
             <p>IP 스피커</p>
-            <li>IP 스피커</li>
-            <li>IP 스피커 서버</li>
-            <li>IP 스피커 방송장치</li>
+            <a href="/product/ip-speaker/ip-speaker">
+              <li>IP 스피커</li>
+            </a>
+            <a href="/product/ip-speaker/ip-speaker-server">
+              <li>IP 스피커 서버</li>
+            </a>
+            <a href="/product/ip-speaker/ip-speaker-broadcast-device">
+              <li>IP 스피커 방송장치</li>
+            </a>
           </ul>
 
           <ul>
             <p>영상 솔루션</p>
-            <li>Octopus Streamer</li>
-            <li>Octopus CMS (영상관제)</li>
+            <a href="/product/video-solution/streamer">
+              <li>Octopus Streamer</li>
+            </a>
+            <a href="/product/video-solution/cms">
+              <li>Octopus CMS (영상관제)</li>
+            </a>
           </ul>
         </ul>
       ) : (
@@ -160,20 +207,34 @@ function Navbar(props) {
         >
           <ul>
             <p>AI 솔루션</p>
-            <li>안전모 감지</li>
-            <li>응용 그라인딩 감지</li>
-            <li>스마트 게이지</li>
+            <a href="/ai/solution/safety-helmet-detection">
+              <li>안전모 감지</li>
+            </a>
+            <a href="/ai/solution/grinding-detection">
+              <li>응용 그라인딩 감지</li>
+            </a>
+            <a href="/ai/solution/smart-gauge">
+              <li>스마트 게이지</li>
+            </a>
           </ul>
 
           <ul>
             <p>AI R&D 실적</p>
-            <li>반도체 웨이퍼 furnace 공정 사고 예측 모니터링</li>
-            <li>도로교량 신축이음 유간 변화 분석</li>
-            <li>P&ID 도면 관계 해석</li>
+            <a href="/ai/r&d/furnace">
+              <li>반도체 웨이퍼 furnace 공정 사고 예측 모니터링</li>
+            </a>
+            <a href="/ai/r&d/expansion-joint">
+              <li>도로교량 신축이음 유간 변화 분석</li>
+            </a>
+            <a href="/ai/r&d/p&id">
+              <li>P&ID 도면 관계 해석</li>
+            </a>
           </ul>
 
           <ul>
-            <p>VISION Engineering</p>
+            <a href="/ai/vision-engineering">
+              <p>VISION Engineering</p>
+            </a>
           </ul>
         </ul>
       ) : (
@@ -188,11 +249,15 @@ function Navbar(props) {
           onMouseLeave={handlePartnerClose}
         >
           <ul>
-            <p>납품실적</p>
+            <a href="/partner/delivery">
+              <p>납품실적</p>
+            </a>
           </ul>
 
           <ul>
-            <p>파트너 소개</p>
+            <a href="/partner/parners">
+              <p>파트너 소개</p>
+            </a>
           </ul>
         </ul>
       ) : (
