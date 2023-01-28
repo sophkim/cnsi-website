f9649166cfa20492d3590d90ed5d5081e74ca0b1
sophkim <soph.b.kim@gmail.com>
2023-01-28 13:36:36 +0900

    이미지 크기 조정

--- src/pages/ai/Furnace.jsx
+++ src/pages/ai/Furnace.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import productImage from "../../assets/AI개발_퍼니스시스템.png";
+import styles from "./test.module.css";
 
 export default function Furnace() {
   return (
     <>
       <Navbar />
-      <img src={productImage} alt="퍼니스시스템"></img>
+      <img src={productImage} className={styles.test} alt="퍼니스시스템"></img>
     </>
   );
 }
