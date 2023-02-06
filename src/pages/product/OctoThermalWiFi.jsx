763998d4c293516a74ca6f19a853c753555679b1
sophkim <soph.b.kim@gmail.com>
2023-02-06 12:01:36 +0900

    버튼 수정

--- src/pages/product/OctoThermalWiFi.jsx
+++ src/pages/product/OctoThermalWiFi.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import productImage from "../../assets/LTE이동형_OctoPortable_Pole.png";
 import styles from "./product.module.css";
 
 export default function OctoThermalWiFi() {
   return (
     <>
       <Navbar />
+      <img
+        src={productImage}
+        className={styles.img}
+        alt="OctoPortable Pole"
+      ></img>
     </>
   );
 }
